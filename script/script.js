ad16846ae621e701d361315453e927f2f0387d15
loozhengjie <loozhengjie555@gmail.com>
2025-05-12 22:01:17 +0800

    save the submit value and displaying it

--- script/script.js
+++ script/script.js
@@ -5,4 +5,8 @@ form.addEventListener("submit", function(e){
     e.preventDefault();
     console.log(e, e.target.elements["task"].value);
 
+    let taskCard = document.createElement("label");
+    taskCard.textContent = e.target.elements["task"].value;
+    document.body.appendChild(taskCard);
+
 })
