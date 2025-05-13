b9b57edb88c4ae3b81bbf50fa940bb297be40d87
loozhengjie <loozhengjie555@gmail.com>
2025-05-13 20:29:57 +0800

    added task cards

--- script/script.js
+++ script/script.js
@@ -1,12 +1,20 @@
 const form = document.querySelector(".input__form");
 const formInput = document.querySelector(".input__field");
 
+const taskContainer = document.querySelector(".tasks");
+
 form.addEventListener("submit", function(e){
     e.preventDefault();
-    console.log(e, e.target.elements["task"].value);
+    const submitValue = e.target.elements["task"].value
+    // console.log(e, e.target.elements["task"].value);
 
-    let taskCard = document.createElement("label");
+    // Check if its empty
+    if (submitValue.trim() == "") return;
+
+    // Create a task card element
+    let taskCard = document.createElement("li");
+    taskCard.classList.add("tasks__card");
     taskCard.textContent = e.target.elements["task"].value;
-    document.body.appendChild(taskCard);
+    taskContainer.appendChild(taskCard);
 
 })
